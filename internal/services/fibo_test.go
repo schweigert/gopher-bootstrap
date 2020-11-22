0dd2766c8543546fac490e7d00483cda0ed88af2
Marlon Henry Schweigert <marlon.schweigert@krakenlab.io>
2020-11-22 11:15:46 -0300

    chore: add tests

---
+++ internal/services/fibo_test.go
@@ -0,0 +1,24 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type FiboSuite struct {
+	suite.Suite
+}
+
+func (suite *FiboSuite) TestNew() {
+	suite.NotNil(NewFibo())
+}
+
+func (suite *FiboSuite) TestFibo() {
+	suite.Equal(uint(0), NewFibo().Fibo(0))
+	suite.Equal(uint(1), NewFibo().Fibo(1))
+}
+
+func TestFiboSuite(t *testing.T) {
+	suite.Run(t, &FiboSuite{})
+}
